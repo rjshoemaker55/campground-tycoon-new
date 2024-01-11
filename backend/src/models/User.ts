fb969323d246dbd5c6158fabcdb02d753312fe72
RJ Shoemaker <rjshoemaker55@gmail.com>
2024-01-11 16:12:22 -0500

    models created

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,16 +1,25 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-interface UserDocument extends Document {
+export interface UserDocument extends Document {
   username: string;
   password: string;
   money: number;
+  lastUpdate: Date;
 }
 
 const userSchema = new Schema({
   username: String,
-  password: String
+  password: String,
+  money: {
+    type: Number,
+    default: 0
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const UserModel = mongoose.model<UserDocument>("User", userSchema, "users");
 
-export default UserModel;
+export { UserModel };
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from "express";
 const router = express.Router();
-import UserModel from "../models/User";
-import ProfileModel from "../models/Profile";
+import { UserModel, UserDocument } from "../models/User";
 
-router.post("/createUser", async (req: Request, res: Response) => {
+// Route: /createUser
+// Create new user
+router.post("/user/createUser", async (req: Request, res: Response) => {
   try {
     const { username, password } = req.body;
 
@@ -16,15 +17,39 @@ router.post("/createUser", async (req: Request, res: Response) => {
       username,
       password
     });
-    const savedUser = await newUser.save();
 
-    // Create corresponding user profile
-    const newProfile = new ProfileModel({ userId: savedUser._id });
-    await newProfile.save();
+    await newUser.save();
 
     res.status(201).json(newUser);
   } catch (err) {
     console.error("Error creating user: ", err);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+// Route: /user/saveGame/:userId
+// Save user's game
+router.post("/saveGame/:userId", async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+
+    // Retrieve user document
+    const user: UserDocument | null = await UserModel.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found." });
+    }
+
+    // Update user's money and last update
+    const { money } = req.body;
+    user.money = money;
+    user.lastUpdate = new Date();
+
+    await user.save();
+
+    res.status(200).json({ message: "Game saved successfully." });
+  } catch (error) {
+    console.error("Error saving game:", error);
     res.status(500).json({ error: "Internal server error." });
   }
 });
